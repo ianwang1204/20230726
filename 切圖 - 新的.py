878ae83b988c6f71fd2de7f634aab429d3ba3353
ianwang1204 <ianwang1204@gmail.com>
2023-07-27 20:16:22 +0800

    deletion and update

--- 切圖 - 新的.py
+++ 切圖 - 新的.py
@@ -1,9 +1,9 @@
 #xmnn, ymnn,xmxn,ymxn=318 547 212 390
 from PIL import Image
 import pandas as pd
-import os, sys
+import os, sys,random
 
-www=open("extra-tests.csv","w")
+www=open("train2.csv","w")
 www.writelines("img, px, py\n")
 
 from PIL import ImageFile
@@ -16,15 +16,18 @@ if image_path not in os.listdir():
 
 data=pd.read_csv("train.csv")
 now=0
-for i in range(5):
+for i in range(data.shape[0]):
     im = Image.open(data.iloc[i][0])
     width, height = im.size
     for j in range(1,9,2):
         x,y=data.iloc[i][j],data.iloc[i][j+1]
-        xx = random.randint(70,170)
-        yy = random.randint(70, 170)
+        xx = random.randint(55,200)
+        yy = random.randint(55, 200)
         box = (x-xx,y-yy,x+256-xx, y+256-yy)
         a = im.crop(box)
         a.save(image_path+"/"+str(now)+".jpg")
         s=f"{now}.jpg,{xx},{yy}\n"
+        print(s)
         www.writelines(s)
+        now+=1
+www.close()
