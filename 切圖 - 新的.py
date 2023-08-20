3c81096c36eba7843aa16af9672a7096279f52cd
ianwang1204 <ianwang1204@gmail.com>
2023-08-20 16:05:53 +0800

    Summary(required)

--- 切圖 - 新的.py
+++ 切圖 - 新的.py
@@ -23,7 +23,7 @@ for i in range(data.shape[0]):
     im = Image.open(data.iloc[i][0])
     width, height = im.size
     for j in range(1,9,2):
-        for k in range(20):
+        for k in range(5):
             x,y=data.iloc[i][j],data.iloc[i][j+1]
             xx = random.randint(aaa,bbb)
             yy = random.randint(aaa,bbb)
